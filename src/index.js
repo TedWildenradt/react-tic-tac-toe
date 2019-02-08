5c23245d6327d09a6e7b5b65258595d979bc2ec1
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-07 20:49:43 -0800

    Delete constructor from square

--- src/index.js
+++ src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      value: null,
-    }
-
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -29,7 +22,7 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={this.handleClick()}/>;
+    return <Square value={this.state.squares[i]} onClick={ () => this.handleClick()}/>;
   }
 
   render() {
